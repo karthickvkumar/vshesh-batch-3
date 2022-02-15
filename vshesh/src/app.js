f0ec03778900f0516ac83644d4ac40e055c2d93d
kvelraj_concord <kvelraj@concord.net>
2022-02-15 10:47:37 +0530

    js

---
+++ vshesh/src/app.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+
+class App extends Component{
+
+   render(){
+    return(
+      <div>
+        <h1>Welcome to App Page</h1>
+        <h2>React JS app file</h2>
+      </div>
+    )
+  }
+}
+
+export default App;
--- vshesh/src/index.js
+++ vshesh/src/index.js
@@ -1,4 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Welcome to React JS</h1>, document.getElementById("root"));
+import App from "./app";
+
+ReactDOM.render(<App></App>, document.getElementById("root"));
