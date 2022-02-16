1d017f8a34ebf73350301b6f29ada5343cde613f
kvelraj_concord <kvelraj@concord.net>
2022-02-16 09:51:19 +0530

    react

--- vshesh/src/app.js
+++ vshesh/src/app.js
@@ -1,12 +1,15 @@
 import React, {Component} from "react";
 
+import SampleComponenet from "./components/sample";
+
 class App extends Component{
 
-   render(){
+  render(){
     return(
       <div>
         <h1>Welcome to App Page</h1>
         <h2>React JS app file</h2>
+        <SampleComponenet></SampleComponenet>
       </div>
     )
   }
