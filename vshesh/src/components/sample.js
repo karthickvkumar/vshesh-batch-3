3915d360e9ead2c60b3563be7113b92ae6e78c1c
kvelraj_concord <kvelraj@concord.net>
2022-02-16 10:18:16 +0530

    react

--- vshesh/src/components/sample.js
+++ vshesh/src/components/sample.js
@@ -2,10 +2,16 @@ import React, {Component} from "react";
 
 class SampleComponenet extends Component{
 
+  display(){
+    let message = "Welcome to dispay method..."
+    console.log(message);
+  }
+
   render(){
     return(
       <div>
         <h1>This is sample Component</h1>
+        <button onClick={ () => this.display() }>Click for Event</button>
       </div>
     )
   }
