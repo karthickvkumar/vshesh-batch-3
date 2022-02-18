58fc41a38e599698a16a1a54f8d1067e881e759f
kvelraj_concord <kvelraj@concord.net>
2022-02-18 10:52:10 +0530

    react

--- vshesh/src/components/login.js
+++ vshesh/src/components/login.js
@@ -6,7 +6,8 @@ class LoginPage extends Component{
     super(props);
     this.state = {
       email_id : "",
-      password : ""
+      password : "",
+      imageStatus : true
     }
   }
 
@@ -21,10 +22,24 @@ class LoginPage extends Component{
     console.log(this.state);
   }
 
+  onShowImage(){
+    this.setState({
+      imageStatus : true
+    })
+  }
+
+  onHideImage(){
+    this.setState({
+      imageStatus : false
+    })
+  }
+
   render(){
+    var name = "Karthick Kumar";
+
     return(
       <div>
-        <h2>Welcome to Login Page</h2>
+        <h2>Welcome to Login Page, {name}</h2>
         <div className="space">
           <label className="label">Please enter your Email ID</label>
           <input className="inputbox" type="text" placeholder="Enter your email id.." onChange={this.onHandleInput} name="email_id"/>
@@ -37,10 +52,23 @@ class LoginPage extends Component{
         <br></br>
         <h2>The email id is {this.state.email_id}</h2>
         <h2>The password is {this.state.password}</h2>
-
-        <img src={require("../images/shinchu.jpg")} className="image"/>
-
+        <div>
+          <button onClick={() => this.onShowImage()}>Show Image</button>
+          <button onClick={() => this.onHideImage()}>Hide Image</button>
+        </div>
+        { this.state.imageStatus && <img src={require("../images/shinchu.jpg")} className="image"/> }
+        
         <img src="https://cdn.motor1.com/images/mgl/mrz1e/s1/coolest-cars-feature.webp" className="image"/>
+
+
+        <div>
+          { false  ? <h2>Successfully Logged in to the Application</h2> 
+                    :
+                    <h2>Authentication Failed</h2>
+          }
+          
+          
+        </div>
 
       </div>
     )
