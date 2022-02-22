2a07344c37a3e53551296a93310642c0a766f1ce
kvelraj_concord <kvelraj@concord.net>
2022-02-22 10:56:44 +0530

    react

--- vshesh/src/components/register.js
+++ vshesh/src/components/register.js
@@ -13,7 +13,16 @@ class RegisterPage extends Component{
       gender : "",
       hobbies : [],
       address : "",
-      city : ""
+      city : "",
+      first_name_error : false,
+      last_name_error : false,
+      email_id_error : false,
+      password_error : false,
+      date_of_birth_error : false,
+      gender_error : false,
+      hobbies_error : false,
+      address_error : false,
+      city_error : false,
     }
   }
 
@@ -42,7 +51,20 @@ class RegisterPage extends Component{
   }
 
   onCreateAccount(){
-    console.log(this.state);
+    
+    this.setState({
+      first_name_error : (this.state.first_name === "") ? true : false,
+      last_name_error : (this.state.last_name === "") ? true : false,
+      email_id_error : (this.state.email_id === "") ? true : false,
+      password_error : (this.state.password === "") ? true : false,
+      date_of_birth_error : (this.state.date_of_birth === "") ? true : false,
+      gender_error : (this.state.gender === "") ? true : false,
+      hobbies_error : (this.state.hobbies.length === 0) ? true : false,
+      address_error : (this.state.address === "") ? true : false,
+      city_error : (this.state.city === "") ? true : false
+    })
+    
+    
   }
 
   render(){
@@ -53,29 +75,35 @@ class RegisterPage extends Component{
           <div className="space">
             <label className="label">First Name</label>
             <input className="inputbox" type="text" placeholder="Enter your first name.." onChange={this.onHandleInput} name="first_name"/>
+            {this.state.first_name_error && <p className="error-msg">Invalid First Name</p>}
           </div>
           <div className="space">
             <label className="label">Last Name</label>
             <input className="inputbox" type="text" placeholder="Enter your last name.." onChange={this.onHandleInput} name="last_name"/>
+            {this.state.last_name_error && <p className="error-msg">Invalid Last Name</p>}
           </div>
         </div>
         <div className="space">
           <label className="label">Please enter your Email ID</label>
           <input className="inputbox" type="text" placeholder="Enter your email id.." onChange={this.onHandleInput} name="email_id"/>
+          {this.state.email_id_error && <p className="error-msg">Invalid Email ID</p>}
         </div>
         <div className="space">
           <label className="label">Please enter your Password</label>
           <input className="inputbox" type="password" placeholder="Enter your password.." onChange={this.onHandleInput} name="password"/>
+          {this.state.password_error && <p className="error-msg">Invalid Password</p>}
         </div>
         <div className="space">
           <label className="label">Select your DOB</label>
           <input className="inputbox" type="date"  onChange={this.onHandleInput} name="date_of_birth"/>
+          {this.state.date_of_birth_error && <p className="error-msg">Invalid Date of Birth</p>}
         </div>
         <div className="space">
           <label className="label">Select your Gender</label>
           <input type="radio" name="gender" onChange={this.onHandleInput} value="Male"/>Male
           <input type="radio" name="gender" onChange={this.onHandleInput} value="Female"/>Female
           <input type="radio" name="gender" onChange={this.onHandleInput} value="Others"/>Others
+          {this.state.gender_error && <p className="error-msg">Invalid Gender</p>}
         </div>
         <div className="space">
           <label className="label">Select your Hobbies</label>
@@ -84,10 +112,12 @@ class RegisterPage extends Component{
           <input type="checkbox" onChange={this.onHandleInput} name="hobbies" value="Vollyball"/>Vollyball
           <input type="checkbox" onChange={this.onHandleInput} name="hobbies" value="Chess"/>Chess
           <input type="checkbox" onChange={this.onHandleInput} name="hobbies" value="Caramboard"/>Caramboard
+          {this.state.hobbies_error && <p className="error-msg">Invalid Hobbies</p>}
         </div>
         <div className="space">
           <label className="label">Please enter your Address</label>
           <textarea className="address" onChange={this.onHandleInput} name="address"></textarea>
+          {this.state.address_error && <p className="error-msg">Invalid Address</p>}
         </div>
         <div className="space">
           <label className="label">Select your City</label>
@@ -98,6 +128,8 @@ class RegisterPage extends Component{
             <option>Banglore</option>
             <option>Mumbai</option>
           </select>
+          {this.state.city_error && <p className="error-msg">Invalid City</p>}
+
         </div>
         <div className="space">
           <button onClick={() => this.onCreateAccount()}>Create New Account</button>
