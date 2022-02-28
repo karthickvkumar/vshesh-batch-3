31e06bc14e6f1f72e81ed02a428f2d36a9ea138a
kvelraj_concord <kvelraj@concord.net>
2022-02-28 10:24:56 +0530

    react

--- vshesh/src/pages/home.js
+++ vshesh/src/pages/home.js
@@ -28,6 +28,17 @@ class HomePage extends Component{
 
   render(){
     console.log(this.state.userList)
+    // shiftkey + alt key + down arrow -> Copy and line and paste it below 
+    var usersListInformation = this.state.userList.map((value, index) => {
+      return (
+        <tr key={index}>
+          <td>{value.first_name}</td>
+          <td>{value.last_name}</td> 
+          <td>{value.email}</td>
+          <td><img src={value.avatar} className="profile-picture" /></td>
+        </tr>
+      );
+    });
 
     return(
       <div>
@@ -51,12 +62,7 @@ class HomePage extends Component{
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>sdfsdf</td>
-              <td>sdfsdf</td>
-              <td>sdfsdf</td>
-              <td>sdfsdf</td>
-            </tr>
+            {usersListInformation}
           </tbody>
         </table>
       </div>
